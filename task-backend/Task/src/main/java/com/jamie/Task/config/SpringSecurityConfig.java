dfa83a869c8abbd8aeb624fcd3c47567c99861ce
Jamie <sirjamiegrant@outlook.com>
2024-09-27 21:57:33 +0100

    In-Memory Authentication
    Learning the basics of In-Memory Authentication.

--- task-backend/Task/src/main/java/com/jamie/Task/config/SpringSecurityConfig.java
+++ task-backend/Task/src/main/java/com/jamie/Task/config/SpringSecurityConfig.java
@@ -2,11 +2,16 @@ package com.jamie.Task.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +19,35 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) ->{
+                .authorizeHttpRequests((authorizeRequests) -> {
                     authorizeRequests.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails Jamie = User.builder()
+                .username("Jamie")
+                .password(passwordEncoder().encode("Test"))
+                .roles("User").build();
+
+        UserDetails Admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN").build();
+
+
+        return new InMemoryUserDetailsManager(Jamie, Admin);
+    }
 }
+
+
