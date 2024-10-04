1167f82c925d076d256a0ea08de2bafa8cf99dc8
Jamie <sirjamiegrant@outlook.com>
2024-10-04 21:22:24 +0100

    changing api urls

--- task-frontend/src/services/TaskService.jsx
+++ task-frontend/src/services/TaskService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getToken} from "./AuthService.jsx";
 
-const REST_API_BASE_URL = 'https://task-env.eba-6nub22wh.eu-west-2.elasticbeanstalk.com/api/tasks'
+const REST_API_BASE_URL = 'https://task-437619.lm.r.appspot.com/api/tasks'
 
 
 // Add a request interceptor
